f8b43b083d43879f40739fe5d685355350b53c49
Brandon <b.garcia0429@outlook.com>
2022-04-24 23:46:00 -0400

    feat(throttle): added throttle method, included with debounce

--- src/functions/fn.debounce.js
+++ src/functions/fn.debounce.js
@@ -9,3 +9,29 @@ bglib.fn.debounce = function(func, delay) {
 		}, delay);
 	};
 };
+
+bglib.fn.throttle = function(func, delay) {
+	//--https://www.youtube.com/watch?v=cjIswDCKgu0
+	var shouldWait = false;
+	var waitingArguments = null;
+	var timeoutFunc = function() {
+		if (waitingArguments == null) {
+			shouldWait = false;
+		}
+		else {
+			func.apply(this, waitingArguments);
+			waitingArguments = null;
+			setTimeout(timeoutFunc, delay);
+		}
+	};
+	return function() {
+		var args = arguments;
+		if (shouldWait) {
+			waitingArguments = args;
+			return;
+		}
+		func.apply(this, args);
+		shouldWait = true;
+		setTimeout(timeoutFunc, delay);
+	}
+};
