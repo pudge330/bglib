82cbc6a5682c76c786973faf0270723cf9b0b184
Brandon <b.garcia0429@outlook.com>
2022-04-24 23:46:59 -0400

    update(formatPrice): use Intl if exists otherwise fallback to old method

--- src/functions/fn.formatPrice.js
+++ src/functions/fn.formatPrice.js
@@ -1,3 +1,18 @@
 bglib.fn.formatPrice = function(amount) {
-    return bglib.fn.formatDecimal(amount, 2);
+    if (bglib.DT.isString(amount)) {
+        amount = parseFloat(amount.replace(/[^\d\.]/g, ''));
+    }
+    if(window.Intl && Intl.NumberFormat) {
+        var formatter = new Intl.NumberFormat('en-US', {
+            roundingMode: 'ceil',
+            currency: 'USD',
+            style: 'currency',
+        });
+        return formatter.format(amount);
+    }
+    return '$' + bglib.fn.formatDecimal(amount, 2);
+};
+
+bglib.fn.formatPriceNumeric = function(amount) {
+    return bglib.fn.formatPrice(amount).replace(/[^\d\.]/g, '');
 };
